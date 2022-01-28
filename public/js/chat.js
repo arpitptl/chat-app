78b80b41ccceeabc6a666071e3a10435b601234d
Arpit Patel <arpit@awaaz.de>
2022-01-28 14:15:15 +0530

    Sending messages and locations to all the users using socket.io

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,77 @@
-io()
+const socket = io()
+
+const messageForm = document.querySelector("#form")
+const messageFormInput = document.querySelector("#input")
+const messageFormButton = document.querySelector("#send-message")
+const messages = document.querySelector("#messages")
+const sendLocationButton = document.querySelector('#send-location')
+
+const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationTemplate = document.querySelector('#location-template').innerHTML
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    messageFormButton.setAttribute('disabled', 'disabled')
+
+    if (messageFormInput.value) {
+        socket.emit('chat message', messageFormInput.value, (error) => {
+            messageFormButton.removeAttribute('disabled')
+            messageFormInput.value = ''
+            messageFormInput.focus()
+            if (error) {
+                return console.log(error)
+            }
+            console.log("Message is delivered!")
+        })
+    }
+})
+
+socket.on('chat message', (msg) => {
+
+    const html = Mustache.render(messageTemplate, {
+        message: msg
+    })
+    messages.insertAdjacentHTML('beforeend', html)
+    // const item = document.createElement('li')
+    // item.textContent = msg
+    // messages.appendChild(item)
+    // window.scrollTo(0, document.body.scrollHeight)
+})
+
+socket.on('user message', (msg) => {
+    const item = document.createElement('li')
+    item.textContent = msg
+    messages.append(item)
+    // window.scrollTo(0, document.body.scrollHeight)
+})
+
+sendLocationButton.addEventListener('click', () => {
+
+    sendLocationButton.setAttribute('disabled', 'disabled')
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser!!')
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position.coords.latitude)
+        console.log(position.coords.longitude)
+        socket.emit('send location', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }, () => {
+            sendLocationButton.removeAttribute('disabled')
+            console.log("Location shared!")
+        })
+    })
+})
+
+socket.on('send location', (location) => {
+    const html = Mustache.render(locationTemplate, {
+        location
+    })
+    messages.insertAdjacentHTML('beforeend', html)
+    // const item = document.createElement('a')
+    // item.textContent = msg
+    // messages.append(item)
+})
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const socketio = require('socket.io')
 const http = require('http')
 const express = require('express')
+const Filter = require('bad-words')
 
 const app = express()
 
@@ -16,8 +17,27 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection', () => {
-    console.log("New Connection")
+io.on('connection', (socket) => {
+    socket.broadcast.emit('user message', "New user has joined!!")
+
+    socket.on('chat message', (msg, callback) => {
+        const filter = new Filter()
+
+        if (filter.isProfane(msg)) {
+            return callback('Profanity is not allowed!')
+        }
+        io.emit('chat message', msg)
+        callback()
+    })
+
+    socket.on('send location', (coords, callback) => {
+        io.emit('send location', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        callback()
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('user message', "A user has left")
+    })
 })
 
 server.listen(port, () => {
